d677c02190691cf33392e3ab408e5e6c22306ca8
Arjun Kumar Verma <27012000arjunmaurya@gmail.com>
2025-06-25 12:43:03 +0530

    Update 26_Scope.py

--- 26_Scope.py
+++ 26_Scope.py
@@ -1,1 +1,18 @@
+# Global scope
+x = "global"
+
+def outer_function():
+    # Enclosing scope
+    x = "enclosing"
+
+    def inner_function():
+        # Local scope
+        x = "local"
+        print("Inner Function (Local Scope):", x)
+
+    inner_function()
+    print("Outer Function (Enclosing Scope):", x)
+
+outer_function()
+print("Global Scope:", x)
 
