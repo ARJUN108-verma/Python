e149e015296c625ffb66f90808d1f4d2f49a8dfd
Arjun Kumar Verma <27012000arjunmaurya@gmail.com>
2025-06-25 12:19:40 +0530

    Update 16_while_loop.py

--- 16_while_loop.py
+++ 16_while_loop.py
@@ -1,1 +1,12 @@
+# Program to demonstrate a while loop in Python
+
+# Initialize counter
+count = 1
+
+# Loop while count is less than or equal to 5
+while count <= 5:
+    print("Count is:", count)
+    count += 1  # Increment count
+
+print("Loop has ended.")
 
