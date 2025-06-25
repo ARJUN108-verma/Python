fc405ad754b2e2d9ea93793f11eb677921ec5fae
Arjun Kumar Verma <27012000arjunmaurya@gmail.com>
2025-06-25 12:16:15 +0530

    Update 14_if-else.py

--- 14_if-else.py
+++ 14_if-else.py
@@ -1,1 +1,13 @@
+# Program to demonstrate if-else in Python
+
+# Input from user
+num = int(input("Enter a number: "))
+
+# if-else logic
+if num > 0:
+    print("The number is positive.")
+elif num == 0:
+    print("The number is zero.")
+else:
+    print("The number is negative.")
 
