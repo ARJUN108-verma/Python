8ac0fb9de3586a2258d7e13c158444802a2e1176
Arjun Kumar Verma <27012000arjunmaurya@gmail.com>
2025-06-25 12:02:03 +0530

    Update 5_Numbers.py

--- 5_Numbers.py
+++ 5_Numbers.py
@@ -1,1 +1,27 @@
+# Integer
+integer_number = 10
+print("Integer:", integer_number)
+
+# Float (Decimal number)
+float_number = 3.14
+print("Float:", float_number)
+
+# Complex Number
+complex_number = 2 + 3j
+print("Complex Number:", complex_number)
+
+# Boolean (also treated as numbers: True = 1, False = 0)
+bool_true = True
+bool_false = False
+print("Boolean (True):", bool_true)
+print("Boolean (False):", bool_false)
+
+# Arithmetic with numbers
+sum_result = integer_number + float_number
+print("Sum of Integer and Float:", sum_result)
+
+# Type Checking
+print("Type of integer_number:", type(integer_number))
+print("Type of float_number:", type(float_number))
+print("Type of complex_number:", type(complex_number))
 
