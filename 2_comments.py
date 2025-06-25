56f0dead18632292f829dbc2ae2f6511d727d5c7
Arjun Kumar Verma <27012000arjunmaurya@gmail.com>
2025-06-25 11:56:50 +0530

    Update 2_comments.py

--- 2_comments.py
+++ 2_comments.py
@@ -1,1 +1,20 @@
+# This is a single-line comment
+# It is used to explain the code below
+
+# Print a greeting
+print("Hello, World!")  # This prints a message to the console
+
+"""
+This is a multi-line comment.
+You can use triple quotes to write comments
+that span multiple lines.
+"""
+
+# A simple math operation
+a = 5
+b = 3
+sum = a + b  # Adding two numbers
+print("Sum:", sum)
+
+# End of program
 
